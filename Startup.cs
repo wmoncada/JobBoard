59a523cd8a4164520636b21e35f621bd82f3739f
Walberto <monposmetal@gmail.com>
2020-12-10 21:05:21 -0600

    Register UseInMemoryDatabase service

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobBoard.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace JobBoard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<JobContext>(
+                options => options.UseInMemoryDatabase(databaseName: "JobBoard")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
