00ef8e6564fd1b845bd4fcfe6f415b48b4a46623
Walberto <monposmetal@gmail.com>
2020-12-10 22:44:31 -0600

    REmove validation from Ids

--- Models/BaseJobObject.cs
+++ Models/BaseJobObject.cs
@@ -5,7 +5,6 @@ namespace JobBoard.Models
     public class BaseJobObject
     {
         [Key]
-        [Required]
         public string Id { get; set; }
         // public virtual string name { get; set; }
 
